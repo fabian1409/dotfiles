5eacbb6aaf5a2e12fe266e1f71b629f28c7c2f4f
Fabian Gruber <fabian.gruber5@gmail.com>
2023-04-10 22:18:06 +0200

    .

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,17 @@
+# starship prompt
 starship init fish | source
+# enable vim mode
 fish_vi_key_bindings
+# remove greeting
 set fish_greeting
+
+# env vars
 export PATH="$HOME/.cargo/bin:$PATH"
 export SUDO_EDITOR="hx"
+
+# alias
 alias .. "cd .."
-alias cat "bat"
+alias cat "bat --theme=OneHalfDark"
 alias ls "exa"
 alias grep "rg"
 alias find "fd"
